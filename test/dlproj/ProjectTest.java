dbf8c2fa571ab1ce1864397ab13ea44324646de5
Andrew Forward <aforward@gmail.com>
2015-01-28 23:39:05 -0500

    Created a basic umple project including access to junit and a dlproj.jar

---
+++ test/dlproj/ProjectTest.java
@@ -0,0 +1,26 @@
+/*
+
+ Copyright: All contributers to the Umple Project
+
+ This file is made available subject to the open source license found at:
+ http://umple.org/license
+
+*/
+
+package dlproj;
+
+import java.io.*;
+import org.junit.*;
+
+public class ProjectTest
+{
+
+  @Test
+  public void constructor()
+  {
+    Assert.assertEquals("myp", new Project("myp").getName());
+  }
+
+}
+
+
